a38575314685185c2317ac2b0877dc53639f7563
patak <90549150+nsbalint@users.noreply.github.com>
2024-05-08 10:40:28 +0200

    Update fitTest.R

--- fitTest.R
+++ fitTest.R
@@ -1,12 +1,15 @@
+#vektor megfigyelt gyakoriságokkal
 k <- c(83, 91, 122, 107, 74, 123)
+#várt relatív gyakoriságok: egyenlőek mert egyenletes eloszlást feltételezünk
 p <- c(1/6, 1/6, 1/6, 1/6, 1/6, 1/6)
 
+#mintnagyság, amely a relatív gyakoriságok százalékos értékeinek meghatározásához szükséges
 N <- 600
 
 Fit <- function(k, p, N) {
 	khi <- 0
 	n <- length(k)
-
+	#Itt számítjuk a Pearson-féle khi értékét a megfigyelt és várt gyakoriságok közötti eltérések alapján
 	for (i in 1 : n) {
 		khi <- khi + (((k[i] - N * p[i])^2)/(N * p[i]))
 	}
